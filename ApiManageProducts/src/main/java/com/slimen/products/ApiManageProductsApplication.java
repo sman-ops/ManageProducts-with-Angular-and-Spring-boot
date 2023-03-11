62c7d9c6d128ae604336fb318810dbb9051e9ea7
sman-ops <slimen.ghenimi@gmail.com>
2023-03-11 22:20:02 +0100

    create two parts of project

---
+++ ApiManageProducts/src/main/java/com/slimen/products/ApiManageProductsApplication.java
@@ -0,0 +1,13 @@
+package com.slimen.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManageProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManageProductsApplication.class, args);
+	}
+
+}
